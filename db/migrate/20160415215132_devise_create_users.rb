e7d7f11b91cf92cb8bc05d084202024b94a156dd
grillopress <grillopress@gmail.com>
2016-04-15 22:10:06 +0000

    add username and job title to user schema

--- db/migrate/20160415215132_devise_create_users.rb
+++ db/migrate/20160415215132_devise_create_users.rb
@@ -1,6 +1,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      
+      ## User attributes
+      
+      t.string :username, null: false, default: ""
+      t.string :job_title,
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -36,6 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username,             unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
