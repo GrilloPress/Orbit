5ae989cd00a34b9cfcacbc10f02e907e40dc1837
grillopress <grillopress@gmail.com>
2016-04-22 20:42:06 +0000

    remove user create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    @user = User.new
+    redirect_to new_user_password_url
+    # @user = User.new
   end
 
   # GET /users/1/edit
@@ -24,17 +25,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to new_user_password_url
   end
 
   # PATCH/PUT /users/1
