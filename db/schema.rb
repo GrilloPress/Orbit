a0ed9b7c5d5bb33275de5637c6f9012f9083c512
grillopress <grillopress@gmail.com>
2016-04-23 21:24:07 +0000

    add biography to user schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418210705) do
+ActiveRecord::Schema.define(version: 20160423212039) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160418210705) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.text     "biography"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
