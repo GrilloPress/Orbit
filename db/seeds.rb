215858d5db853b3eeb83cdf97008c6f7657a36b6
grillopress <grillopress@gmail.com>
2016-04-25 21:25:31 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_list = [
+  [ "Mr_Turbo", "Race car driver", "password1", "turbo@example.com"],
+  [ "Ms_Rocket", "Rocket Engineer", "password1", "rocket@example.com"],
+  [ "Guy_Incognito", "Space Pirate", "password1", "guy@example.com"],
+  [ "Marketing_Space_Guy", "Marketing", "password1", "marketing@example.com"],
+
+]
+
+user_list.each do |username, job_title, password, email|
+  User.create( username: username, job_title: job_title, password: password, email: email )
+end
