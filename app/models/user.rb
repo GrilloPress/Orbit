11a335688c8c2c98d154a4bd91704e7001ce9a8b
grillopress <grillopress@gmail.com>
2016-04-26 21:14:22 +0000

    add username validation and transformation to underscores

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # paperclip
   
+  before_save :username_to_snake_case
+  
+  # paperclip
   has_attached_file :profile_pic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_pic, content_type: /\Aimage\/.*\Z/
+  
+  # validations
+  validates :biography, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :username, length: { minimum: 5 }
+  validates :username, uniqueness: { case_sensitive: false }
+  
+  protected
+  
+  def username_to_snake_case
+    
+    self.username = username.tr(" ", "_")
+
+  end
   
 end
