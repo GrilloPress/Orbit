df48c843630f4ca97415963946adb2018e38443d
grillopress <grillopress@gmail.com>
2016-05-11 21:29:47 +0000

    add static page index dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   
   def index
+    @users = User.all.order(:username) # user first(10) instead of .all in a real app...
+    @departments = Department.all.order(:title)
   end
   
 end
