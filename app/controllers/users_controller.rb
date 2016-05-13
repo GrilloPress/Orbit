846a91412b7b53b3e8c201bd032c76a18ccd24ef
grillopress <grillopress@gmail.com>
2016-05-13 09:49:42 +0000

    add user department title to user show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    
+    @department = Department.find(@user.department_id)
   end
 
   # GET /users/new
